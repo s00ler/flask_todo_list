9039077006d5b889b39881d4cf554ba4c6ab477d
s00ler <d.a.gerasimov.94@gmail.com>
2018-01-27 15:13:54 +0300

    refactor

--- application/forms/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email
-
-
-class LoginForm(FlaskForm):
-    email = StringField('email', validators=[Email(), DataRequired])
-    password = PasswordField('password', validators=[DataRequired()])
-    submit = SubmitField('submit')
--- application/server/readconfig.py
+++
@@ -1,4 +0,0 @@
-from configparser import ConfigParser
-
-configuration = ConfigParser()
-configuration.read('./config.ini')
